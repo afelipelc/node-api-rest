c972f64098bdebd7e5617c4ae969eddd26b660e6
AFelipe <afelipelc@gmail.com>
2019-08-21 10:39:07 -0500

    create firs schema

---
+++ models/Clients.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const clientsSchema = new Schema({
+  name: {
+    type: String,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    trim: true
+  },
+  business: {
+    type: String,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  phone: {
+    type: String,
+    trim: true
+  }
+});
+
+module.exports = mongoose.model('Clients', clientsSchema);
