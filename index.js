9b4b440f98ec327f0f6fe5d3858e73225f47981f
AFelipe <afelipelc@gmail.com>
2019-08-21 10:28:18 -0500

    add mongoose in project

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const mongoose = require('mongoose');
+
+// conect mongodb
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/restapi', {
+  useNewUrlParser: true
+})
 
 // create server
 const app = express();
