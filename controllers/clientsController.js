8684d4a5bec6642ed8fe31ee9814d211f116e14e
AFelipe <afelipelc@gmail.com>
2019-08-21 11:59:56 -0500

    clients crud implemented

--- controllers/clientsController.js
+++ controllers/clientsController.js
@@ -25,3 +25,59 @@ exports.clients = async (req, res) => {
     next();
   }
 };
+
+
+// get client by :id
+exports.show = async (req, res, next) => {
+  try {
+    const client = await Clients.findById(req.params.id);
+    if (!client) {
+    res.json({
+      message: 'Client doesn\'t exists'
+    });
+    next();
+    }
+
+    res.json(client);
+  } catch (error) {
+    console.log(error);
+    res.json({
+      message: 'Error, check your info'
+    });
+    next();
+  }
+};
+
+// put: update client
+exports.update = async (req, res, next) => {
+  try {
+    const client = await Clients.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true } // return updated
+    );
+    res.json(client);
+  } catch (error) {
+    console.log(error);
+    res.json({
+      message: 'Error, check your sended info'
+    });
+    next();
+  }
+};
+
+// delete: client
+exports.delete = async (req, res, next) => {
+  try {
+    await Clients.findOneAndDelete({ _id: req.params.id });
+    res.json({
+      message: 'Client was deleted'
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      message: 'Error, check your sended info'
+    });
+    next();
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,13 @@ const clientsController = require('../controllers/clientsController');
 module.exports = function() {
   // post: add new client
   router.post('/clients', clientsController.addClient);
+  // get: all clients
   router.get('/clients', clientsController.clients);
-
+  // get: client by :id
+  router.get('/clients/:id', clientsController.show);
+  // put: update client
+  router.put('/clients/:id', clientsController.update);
+  // delete: client
+  router.delete('/clients/:id', clientsController.delete);
   return router;
 }
